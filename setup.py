f82879bf70dd7d6f9569cf32a9e2af88efe60029
Babak Farrokhi <babak@farrokhi.net>
2016-04-30 15:41:28 +0430

    add setuptools support

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+setup(
+    name = "dnsdiag",
+    version = "1.1",
+    packages = find_packages(),
+    scripts = ['dnsping.py', 'dnstraceroute.py', 'dnseval.py'],
+    classifiers=[
+        "Topic :: System :: Networking",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+    ],
+
+    install_requires = ['cymruwhois>=1.4'],
+    author = "Babak Farrokhi",
+    author_email = "babak@farrokhi.net",
+    description = "DNS Diagnostics and measurement tools (ping, traceroute)",
+    long_description = """
+DNSDiag provides a handful of tools to measure and diagnose your DNS
+performance and integrity. Using dnsping, dnstraceroute and dnseval tools
+you can measure your DNS response quality from delay and loss perspective
+as well as tracing the path your DNS query takes to get to DNS server.
+""",
+    license = "BSD",
+    keywords = "dns traceroute ping",
+    url = "https://github.com/farrokhi/dnsdiag",
+)
