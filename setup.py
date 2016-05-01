f6df2e71c6a7d0f08a0556c6332ef3f3502971bc
Babak Farrokhi <babak@farrokhi.net>
2016-05-01 23:06:02 +0430

    bump version again

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "dnsdiag",
-    version = "1.2.3",
+    version = "1.2.4",
 	packages = find_packages(),
     install_requires = ['cymruwhois==1.5'],
     dependency_links = [
