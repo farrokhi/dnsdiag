4dd5065c73b954f4f51378b22fd77cde947bcfde
Babak Farrokhi <babak@farrokhi.net>
2016-05-16 19:05:48 +0430

    fix README and code cleanup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+
 setup(
-    name = "dnsdiag",
-    version = "1.3.5",
-	packages = find_packages(),
+    name="dnsdiag",
+    version="1.3.5",
+    packages=find_packages(),
     classifiers=[
         "Topic :: System :: Networking",
         "Environment :: Console",
@@ -13,20 +14,20 @@ setup(
         "Operating System :: OS Independent",
     ],
 
-    author = "Babak Farrokhi",
-    author_email = "babak@farrokhi.net",
-    description = "DNS Diagnostics and measurement tools (ping, traceroute)",
-    long_description = """
+    author="Babak Farrokhi",
+    author_email="babak@farrokhi.net",
+    description="DNS Diagnostics and measurement tools (ping, traceroute)",
+    long_description="""
 DNSDiag provides a handful of tools to measure and diagnose your DNS
 performance and integrity. Using dnsping, dnstraceroute and dnseval tools
 you can measure your DNS response quality from delay and loss perspective
 as well as tracing the path your DNS query takes to get to DNS server.
 """,
-    license = "BSD",
-    keywords = "dns traceroute ping",
-    url = "https://dnsdiag.org/",
+    license="BSD",
+    keywords="dns traceroute ping",
+    url="https://dnsdiag.org/",
     entry_points={
-        'console_scripts':[
+        'console_scripts': [
             'dnsping = dnsping:main',
             'dnstraceroute = dnstraceroute:main',
             'dnseval = dnseval:main',
