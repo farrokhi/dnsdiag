347881f944ea8a6007515d6ec01cac81951d135e
Babak Farrokhi <babak@farrokhi.net>
2016-06-15 16:27:27 +0430

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dnsdiag",
-    version="1.4.0",
+    version="1.4.1",
     packages=find_packages(),
     classifiers=[
         "Topic :: System :: Networking",
