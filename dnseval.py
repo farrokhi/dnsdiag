e1c508cea7c1c2d42322ce33b36a8dd1ea9a613f
Babak Farrokhi <babakfarrokhi@optiver.com>
2020-01-17 12:59:47 +0100

    Update copyright

--- dnseval.py
+++ dnseval.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2016, Babak Farrokhi
+# Copyright (c) 2020, Babak Farrokhi
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
