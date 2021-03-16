aede7146667591b73d4b02ca415454f1ad7981dd
Babak Farrokhi <babak@farrokhi.net>
2021-03-16 14:05:00 +0100

    Add Python 3.8

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: Name Service (DNS)",
         "Development Status :: 5 - Production/Stable",
