8c72ebe214cb7175e80014e5f3c336941382dedf
Babak Farrokhi <babak@farrokhi.net>
2021-04-05 18:42:29 +0200

    Improve windows support
    - Add windows compatibility shims for virtualenv (because windows is
      always fun)
    - Remove upx support. Slight compression is not worth extraction
      overhead.
    - Improve script to be compatible with POSIX sh

--- build-pkgs.sh
+++ build-pkgs.sh
@@ -14,48 +14,51 @@ msg() {
 }
 
 checkbin() {
-    which ${1} > /dev/null 2>&1 || die "${1} is not installed"
+    which "${1}" > /dev/null 2>&1 || die "${1} is not installed"
 }
 
 ## validate required tools
 checkbin "virtualenv"
 checkbin "python3"
-checkbin "upx"
 
 ## constants
-ARCH=`uname -m`
-PLATFORM=`uname -s | tr 'A-Z' 'a-z'`
-DDVER=`grep version setup.py | awk -F\" '{print $2}'`
+ARCH=$(uname -m)
+PLATFORM=$(uname -s | tr 'A-Z' 'a-z')
+DDVER=$(grep version setup.py | awk -F\" '{print $2}')
 PKG_NAME="dnsdiag-${DDVER}.${PLATFORM}-${ARCH}-bin"
 PKG_PATH="pkg/${PKG_NAME}"
 
 ## main
 msg "Creating virtualenv"
 virtualenv -q --clear .venv
-. ./.venv/bin/activate
+if [ -f .venv/bin/activate ]; then  # *nix
+  . .venv/bin/activate
+elif [ -f .venv/Scripts/activate ]; then  # windows
+  . .venv/Scripts/activate
+fi
 
 msg "Installing dependencies"
 pip3 install -q pyinstaller || die "Failed to install pyinstaller"
 pip3 install -q -r requirements.txt || die "Failed to install dependencies"
 
-mkdir -p ${PKG_PATH} || die "Cannot create dir hierarcy: ${PKG_PATH}"
+mkdir -p "${PKG_PATH}" || die "Cannot create dir hierarcy: ${PKG_PATH}"
 
 for i in dnsping.py dnstraceroute.py dnseval.py; do
     msg "Building package for ${i}"
     pyinstaller ${i} -y --onefile --clean \
         --log-level=ERROR \
-        --distpath=${PKG_PATH} \
+        --distpath="${PKG_PATH}" \
         --hidden-import=dns \
         --hidden-import=requests
 done
 
 msg "Adding extra files..."
 for i in public-servers.txt public-v4.txt rootservers.txt; do
-    cp ${i} ${PKG_PATH}/
+    cp ${i} "${PKG_PATH}/"
 done
 
 msg "Building tarball: ${PKG_NAME}.tar.gz"
 cd pkg
-tar cf ${PKG_NAME}.tar ${PKG_NAME} || die "Failed to build archive (tar)"
-gzip -9f ${PKG_NAME}.tar           || die "Failed to build archive (gzip)"
-rm -fr ${PKG_NAME}
+tar cf "${PKG_NAME}".tar "${PKG_NAME}" || die "Failed to build archive (tar)"
+gzip -9f "${PKG_NAME}.tar"             || die "Failed to build archive (gzip)"
+rm -fr "${PKG_NAME}"
