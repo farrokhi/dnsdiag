8e4f97606d1d9ce9d841593079d200987c8877c9
Babak Farrokhi <babak@farrokhi.net>
2021-05-12 12:02:49 +0200

    Revert a missing constant (fixes #88)

--- dnseval.py
+++ dnseval.py
@@ -51,6 +51,7 @@ class Colors(object):
     N = '\033[m'  # native
     R = '\033[31m'  # red
     G = '\033[32m'  # green
+    O = '\033[33m'  # orange
     B = '\033[34m'  # blue
 
     def __init__(self, mode):
