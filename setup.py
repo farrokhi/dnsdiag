ad59c248f08db16d724c60a6185071e07ccde9ef
Babak Farrokhi <babak@farrokhi.net>
2023-02-01 18:16:53 +0100

    Update supported python version

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: Name Service (DNS)",
         "Development Status :: 5 - Production/Stable",
